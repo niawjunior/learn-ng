d3a32fecb714f5986fb6d6edec61c1979803fc43
niawkung <niawkung2@gmail.com>
2018-11-05 17:17:02 +0700

    add multi component

---
+++ src/app/guest-details/guest-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-details',
+  templateUrl: './guest-details.component.html'
+})
+export class GuestDetailsComponent {
+  title = 'Hello Child Component';
+}
